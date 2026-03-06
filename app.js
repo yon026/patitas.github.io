f150f1b4ab59b000bd90dfd6130bdda9bf4bb5d9
yon026 <142118611+yon026@users.noreply.github.com>
2026-03-05 22:12:48 -0300

    Update app.js
    quita de comentarios

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Reemplaza los textos entre comillas con tus datos reales de Supabase
+
 const SUPABASE_URL = 'https://tedswrqzjdfxhvdyszmj.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZHN3cnF6amRmeGh2ZHlzem1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NTU0MDgsImV4cCI6MjA4ODMzMTQwOH0.0WXKTZB_-DgHAZ2g5CHmISnnSjJNZWwoHBHYiDicNuQ';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// Prueba rápida para ver en la consola si funciona
+
 async function verificar() {
     const { data, error } = await supabase.from('servicios').select('*');
     if (error) {
