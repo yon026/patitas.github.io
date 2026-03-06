ae881a3be694adf66fc3226c62542540656ad26f
yon026 <142118611+yon026@users.noreply.github.com>
2026-03-05 22:23:43 -0300

    Update app.js
    Cambio de variable de supabase por repeticion de datos

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const SUPABASE_URL = 'https://tedswrqzjdfxhvdyszmj.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZHN3cnF6amRmeGh2ZHlzem1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NTU0MDgsImV4cCI6MjA4ODMzMTQwOH0.0WXKTZB_-DgHAZ2g5CHmISnnSjJNZWwoHBHYiDicNuQ';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
 async function verificar() {
-    const { data, error } = await supabase.from('servicios').select('*');
+    const { data, error } = await db.from('servicios').select('*');
     if (error) {
-        console.log("Error de conexión:", error.message);
+        console.log("❌ Error:", error.message);
     } else {
         console.log("✅ Conexión establecida. Servicios:", data);
     }
