6999aefc9c41512da8744e16c4a5f075b389d0e8
yon026 <142118611+yon026@users.noreply.github.com>
2026-03-08 23:08:42 -0300

    cambio de variable db por supabaseCliente
    Resolucion de conflictos con nombres de variables

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const SUPABASE_URL = 'https://tedswrqzjdfxhvdyszmj.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZHN3cnF6amRmeGh2ZHlzem1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NTU0MDgsImV4cCI6MjA4ODMzMTQwOH0.0WXKTZB_-DgHAZ2g5CHmISnnSjJNZWwoHBHYiDicNuQ';
 
-const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
 async function verificar() {
-    const { data, error } = await db.from('servicios').select('*');
+    const { data, error } = await supabaseClient.from('servicios').select('*');
     if (error) {
         console.log("❌ Error:", error.message);
     } else {
@@ -21,18 +21,18 @@ async function manejarAuth(tipo) {
     const password = document.getElementById('password').value;
 
     if (tipo === 'registro') {
-        const { data, error } = await db.auth.signUp({ email, password });
+        const { data, error } = await supabaseClient.auth.signUp({ email, password });
         if (error) return alert("Error: " + error.message);
         alert("¡Registro exitoso! Ya puedes iniciar sesión.");
         location.reload(); // Recarga para volver al formulario de login
     } else {
         // 1. Iniciar sesión en el sistema de Auth
-        const { data: authData, error: authError } = await db.auth.signInWithPassword({ email, password });
+        const { data: authData, error: authError } = await supabaseClient.auth.signInWithPassword({ email, password });
         
         if (authError) return alert("Error: " + authError.message);
 
         // 2. Buscar el ROL en nuestra tabla de perfiles usando el ID del usuario logueado
-        const { data: profile, error: profileError } = await db
+        const { data: profile, error: profileError } = await supabaseClient
             .from('profiles')
             .select('role')
             .eq('id', authData.user.id)
